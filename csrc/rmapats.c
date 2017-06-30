006226bcb06a83de6d70d678aa21763b0f8f3ca1
Qiang Zhao-B51816 <b51816@lvd5754.ap.freescale.net>
2017-06-30 08:19:30 +0530

    .

---
+++ csrc/rmapats.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rmapats.h"
+
+scalar dummyScalar;
+scalar fScalarIsForced=0;
+scalar fScalarIsReleased=0;
+scalar fScalarHasChanged=0;
+void  hsF_0(struct dummyq_struct * I659, EBLK  * I660, U  I508);
+#ifdef __cplusplus
+extern "C" {
+#endif
+void SinitHsimPats(void);
+#ifdef __cplusplus
+}
+#endif
--- jay_test/base_test.sv
+++ jay_test/base_test.sv
@@ -7,6 +7,7 @@ class base_test extends uvm_test;
 
    extern virtual function void build_phase(uvm_phase phase);
    extern virtual function void report_phase(uvm_phase phase);
+   extern virtual function void phase_started(uvm_phase phase);
    extern virtual task main_phase(uvm_phase phase);
    virtual function void connect_phase(uvm_phase phase);
       super.connect_phase(phase);
@@ -20,6 +21,13 @@ function void base_test::build_phase(uvm_phase phase);
    super.build_phase(phase);
    env = zmk_env::type_id::create("env",this);
    uvm_config_db#(uvm_object_wrapper)::set(this,"env.i_agt.sqr.main_phase","default_sequence",zmk_sequence_0::type_id::get());
+endfunction
+
+function void base_test::phase_started(uvm_phase phase);
+`uvm_info("phase_started",$sformatf("current phase name is %s",phase.get_name()),UVM_LOW);
+   if(phase.get_name() == "main") begin
+      `uvm_info("BASE_TEST","Before Main Phase",UVM_LOW);
+   end
 endfunction
 
 function void base_test::report_phase(uvm_phase phase);
@@ -39,6 +47,7 @@ endfunction
 
 task base_test::main_phase(uvm_phase phase);
    super.main_phase(phase);
+   //phase_started(phase);   // no need to call
    phase.raise_objection(this);
    #20000;
    phase.drop_objection(this);
